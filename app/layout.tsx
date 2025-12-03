1c1c19fc630b9393dbe4c12085e1f66d9b70c57c
masoume <masomehrezaei83.r@gmail.com>
2025-12-03 08:57:01 +0330

    completing the layout and ProductCard.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -27,7 +28,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+          <div className="mx-auto p-4 sm:max-w-xl md:max-w-xl lg:max-w-3xl xl:max-w-7xl ">
+           <Navbar/> {children} <Footer/>
+        </div>
       </body>
     </html>
   );
---
+++ components/ProductCard.tsx
@@ -0,0 +1,72 @@
+'use client'
+import {ProductType} from "@/types";
+import Link from "next/link";
+import Image from "next/image";
+import {ShoppingCart} from "lucide-react";
+import {useState} from "react";
+import {type} from "node:os";
+
+const ProductCard=({product}:{product:ProductType})=>{
+    const [productTypes,setProductTypes]=useState({
+        size:product.sizes[0],
+        color:product.colors[0],
+    })
+    const handleProductType=({type,value}:{type:'size'| 'color',value:string})  => {
+         setProductTypes((prev)=>({
+             ...prev,
+             [type]:value,
+         }))
+    }
+    return (
+        <div className={'shadow-lg rounded-lg overflow-hidden'}>
+           {/*IMAGE*/}
+            <Link href={`/products/${product.id}`}>
+                <div className={' relative aspect-[2/3]'}>
+                    <Image src={product.images[productTypes.color]} alt={product.name} fill  className={'object-cover hover:scale-105 transition-all duration-300'}/>
+                </div>
+            </Link>
+            {/*Product detail*/}
+            <div className={'flex flex-col gap-4 p-4'}>
+                <h1 className={'font-medium'}>{product.name}</h1>
+                <p className={'text-sm text-gray-500'}>{product.shortDescription}</p>
+                {/*PRODUCT DETAIL*/}
+                <div className={'flex items-center gap-4 text-xs'}>
+                    {/*SIZE*/}
+                    <div className={'flex flex-col gap-1'}>
+                        <span className={'text-gray-500'}>Size</span>
+                        <select name={'size'}
+                                id={'size'}
+                                onChange={e=>handleProductType({type:'size',value:e.target.value})}
+                                className={'ring ring-gray-300 rounded-md px-2 py-1'}>
+                            {product.sizes.map(size => (
+                                <option className={' bg-gray-700 text-white hover:bg-green-400'} key={size} value={size}>{size.toUpperCase()}</option>
+                            ))}
+                        </select>
+                    </div>
+                    {/*colors*/}
+                    <div className={'flex flex-col'}>
+                        <span className={'text-gray-500'}>Color</span>
+                        <div className={'flex items-center gap-2'}>
+                            {product.colors.map(color => (
+                                <div className={`cursor-pointer border-1 ${productTypes.color === color ? 'border-gray-400' : 'border-gray-200'} rounded-full p-[1.2px]`}
+                                     key={color}
+                                     onClick={()=>handleProductType({type:'color',value:color})}
+                                >
+                                    <div className={'w-[14px] h-[14px] rounded-full'} style={{backgroundColor:color}}/>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                </div>
+                {/*PRICE AND ADD TO CART*/}
+                <div className={'flex items-center justify-between'}>
+                    <p className={'font-medium'}>${product.price.toFixed(2)}</p>
+                    <button className={'ring-1 ring-gray-200 shadow-lg rounded-md px-2 py-1 text-sm cursor-pointer hover:text-white hover:bg-black transition-all duration-300 flex items-center gap-2'}>
+                        <ShoppingCart  className={'h-4 w-4'}/>
+                        Add to cart</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default ProductCard;
