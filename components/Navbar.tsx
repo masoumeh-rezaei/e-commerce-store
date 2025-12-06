cfd294019afc8fdc1d4b5d826e98736c27c30ab5
masoume <masomehrezaei83.r@gmail.com>
2025-12-06 11:57:54 +0330

    update navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import Image from "next/image";
 import SearchBar from "@/components/SearchBar";
 import {Bell, Home, ShoppingCart} from "lucide-react";
+import ShoppingCartIcon from "@/components/ShoppingCartIcon";
 
 const Navbar = ()=>{
     return(
@@ -22,7 +23,7 @@ const Navbar = ()=>{
                 <Home className={'w-4 h-4 text-gray-600'}/>
                 </Link>
                 <Bell className={'w-4 h-4 text-gray-600'}/>
-                <ShoppingCart className={'w-4 h-4 text-gray-600'}/>
+                <ShoppingCartIcon/>
                 <Link href={'/login'}>Login</Link>
             </div>
         </nav>
